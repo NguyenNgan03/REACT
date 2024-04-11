f9574edba6c4d644ac3ea3748b3bc53b93803bd3
NguyenNgan03 <simple.270123@gmail.com>
2024-04-11 22:57:52 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+let numbers = [1,2,3,4,5];
+numbers.push(6);
+numbers.forEach((element)=> console.log(element));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let index = 2;
+numbers.at(index);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+numbers.slice(1);
+numbers.reverse();
+numbers.filter((number)=>number>5 );
+
